7a062f45aadfc59396cd75eb3ba1f425eda4d116
ningpop <craft1933@naver.com>
2021-11-04 17:45:56 +0900

    ➕[ADD] 수정된 Model에 따른 List View 구현

--- study/views.py
+++ study/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Study
 
 # Create your views here.
 
 def study_list(request):
-    return render(request, 'study_list.html')
+    study_query_set = Study.objects.order_by('pub_date')
+    context = { 'studies' : study_query_set }
+    return render(request, 'study_list.html', context)
 
 def study_detail(request):
     return render(request, 'study_detail.html')
