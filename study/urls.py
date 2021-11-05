4c903712ffdeb23431b8b71ca57dafe5cf03c711
ningpop <craft1933@naver.com>
2021-11-06 03:08:56 +0900

    ➕[[ADD] create 작업 완료

--- study/urls.py
+++ study/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('list/', views.study_list, name='study_list'),
     path('detail/<int:study_id>/', views.study_detail, name='study_detail'),
+    path('create/', views.study_create, name='study_create'),
 ]
