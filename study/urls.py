af705967c71bd746b65acf16e8ffd0be078838ab
ningpop <craft1933@naver.com>
2021-11-07 15:39:45 +0900

    ➕[ADD] update 작업 완료

--- study/urls.py
+++ study/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('list/', views.study_list, name='study_list'),
     path('detail/<int:study_id>/', views.study_detail, name='study_detail'),
     path('create/', views.study_create, name='study_create'),
+    path('update/<int:study_id>/', views.study_update, name='study_update'),
 ]
